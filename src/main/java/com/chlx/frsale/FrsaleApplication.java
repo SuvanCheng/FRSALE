bc82284552b3284dfc8dfb0470d68887cb6723e4
suvancheng <suvancheng@outlook.com>
2021-05-09 11:46:21 +0800

    init

---
+++ src/main/java/com/chlx/frsale/FrsaleApplication.java
@@ -0,0 +1,13 @@
+package com.chlx.frsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrsaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrsaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chlx/frsale/FrsaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chlx.frsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrsaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
